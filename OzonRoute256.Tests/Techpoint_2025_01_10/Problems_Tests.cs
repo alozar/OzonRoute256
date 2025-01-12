b71763876b30d77e710dcc53617fc33dd5c13ddf
alozar <klimovforever.92@mail.ru>
2025-01-12 19:12:38 +0800

    Создание проекта по контестам Ozon. Решение задачи 1 "Удалить цифру из числа".

---
+++ OzonRoute256.Tests/Techpoint_2025_01_10/Problems_Tests.cs
@@ -0,0 +1,41 @@
+﻿using OzonRoute256.Techpoint_2025_01_10;
+
+namespace OzonRoute256.Tests.Techpoint_2025_01_10;
+
+public class Problems_Tests
+{
+    public static IEnumerable<object[]> GetDataDigitRemove()
+    {
+        List<object[]> data = new();
+
+        int t = 1;
+        string[] sums = new[] { "200000" };
+        string[] expected = new[] { "20000" };
+        data.Add(new object[] { t, sums, expected });
+
+        t = 3;
+        sums = new[] { "9", "0", "9123" };
+        expected = new[] { "0", "0", "923" };
+        data.Add(new object[] { t, sums, expected });
+
+        t = 2;
+        sums = new[] { "987654321", "9611511" };
+        expected = new[] { "98765432", "961511" };
+        data.Add(new object[] { t, sums, expected });
+
+        t = 3;
+        sums = new[] { "984724", "32313", "34322" };
+        expected = new[] { "98724", "3313", "4322" };
+        data.Add(new object[] { t, sums, expected });
+        return data;
+    }
+
+    [Theory]
+    [MemberData(nameof(GetDataDigitRemove))]
+    public void DigitRemove_Success(int t, string[] sums, string[] expected)
+    {
+        Problems.DigitRemove(t, sums);
+
+        Assert.Equal(expected, sums);
+    }
+}
---
+++ OzonRoute256/Techpoint_2025_01_10/Problems.cs
@@ -0,0 +1,67 @@
+﻿namespace OzonRoute256.Techpoint_2025_01_10;
+
+public class Problems
+{
+    /// <summary>
+    /// 1. Удалить цифру из числа
+    /// </summary>
+    public static void DigitRemove(int t, string[] sums)
+    {
+        for (int i = 0; i < sums.Length; i++)
+        {
+            if (sums[i].Length == 1)
+            {
+                sums[i] = "0";
+                continue;
+            }
+
+            char[] chars = new char[sums[i].Length - 1];
+            bool changed = false;
+            for (int j = 0, k = 0; j < sums[i].Length; j++, k++)
+            {
+                if (!changed && j + 1 < sums[i].Length && sums[i][j] < sums[i][j + 1])
+                {
+                    chars[k] = sums[i][j + 1];
+                    j++;
+                    changed = true;
+                    continue;
+                }
+
+                if (k < sums[i].Length - 1)
+                {
+                    chars[k] = sums[i][j];
+                }
+            }
+
+            sums[i] = new(chars);
+        }
+    }
+
+    /// <summary>
+    /// 2. Валидация ответа
+    /// </summary>
+    public static void ValidateOutput()
+    {
+    }
+
+    /// <summary>
+    /// 3. Ваня и вирусный файл (C#)
+    /// </summary>
+    public static void VirusFilesCs()
+    {
+    }
+
+    /// <summary>
+    /// 4. Планировщик заказов
+    /// </summary>
+    public static void OrderPlanner()
+    {
+    }
+
+    /// <summary>
+    /// 5. ASCII-роботы
+    /// </summary>
+    public static void ASCIIRobots()
+    {
+    }
+}
